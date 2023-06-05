5b05a06b6c8bc9f603bf3553599eb6d6649f21ec
tinamorlock <tinamorlock@gmail.com>
2023-06-05 10:51:53 -0500

    added search filter to get posts

--- routers/post.py
+++ routers/post.py
@@ -3,7 +3,7 @@ from . import oauth2
 from . .database import get_db
 from sqlalchemy.orm import Session
 from fastapi import Response, status, HTTPException, Depends, APIRouter
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/posts",
@@ -11,12 +11,13 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, 
+              search: Optional[str] = ""):
     
     # This only shows logged in user's posts
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     print(limit)
-    posts = db.query(models.Post).limit(limit).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
