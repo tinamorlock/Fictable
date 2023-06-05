9a7246972e59fbd10ea6d17ba6f9d77e10a4a8cf
tinamorlock <tinamorlock@gmail.com>
2023-06-05 10:56:38 -0500

    added comments to main

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from .routers import post, user, auth
 from . import models 
 from .database import engine
 
+# this ensures the sql alchemy models are implemented and created on the database
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# add more routers here when you add more functionality
 
 app.include_router(post.router)
 app.include_router(user.router)
